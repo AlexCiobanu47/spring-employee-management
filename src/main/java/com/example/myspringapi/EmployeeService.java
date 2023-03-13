92bf37366d6a4201aa9d0e6851f508c0d5c48f75
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-03-13 23:58:24 +0200

    Added service layer

---
+++ src/main/java/com/example/myspringapi/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.myspringapi;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List <Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
+}
